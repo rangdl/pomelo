2f074361ce6d3ab8c19e3b75af70e022a838b4d0
rang <378694192@qq.com>
2026-06-03 17:48:33 +0800

    888

--- cli/commands/build/ios.dart
+++ cli/commands/build/ios.dart
@@ -22,7 +22,6 @@ class IosBuildCommand extends Command with BuildCommandCommonSteps {
       flutter build ios --release --no-codesign --flavor ${CliEnv.channel.name}
       ln -sf $buildDirPath Payload
       ls -la $buildDirPath
-      ls -la Payload
       zip -r9 Pomelo-iOS.ipa ${join("Payload", "Runner.app")}
       """);
     // zip -r9 Pomelo-iOS.ipa ${join("Payload", "${CliEnv.channel.name}.app")}
