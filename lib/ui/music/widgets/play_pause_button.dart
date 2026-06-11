81960c6e77c980fd942d88a33c99aeb80ab7f066
rang <378694192@qq.com>
2026-06-11 17:40:11 +0800

    chore: 修复

--- lib/ui/music/widgets/play_pause_button.dart
+++ lib/ui/music/widgets/play_pause_button.dart
@@ -1,6 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pomelo/modules/audio_player/module_providers.dart';
-import 'package:pomelo/modules/audio_player/providers/audio_player.dart';
 import 'package:pomelo/modules/music/model/song.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
